c8e7ac9dba0e79b0346dd88061ed0a0597ce73a9
Matheus153 <santiosmatheus@gmail.com>
2025-05-09 14:38:43 -0400

    Fix: aceitando atraso na busca pelo token de autenticação

--- app/routes.py
+++ app/routes.py
@@ -302,7 +302,7 @@ def login():
 def login_social():
     try:
         id_token = request.json.get('token')
-        decoded_token = firebase_auth.verify_id_token(id_token)
+        decoded_token = firebase_auth.verify_id_token(id_token, clock_skew_seconds=60)
         user_id = decoded_token['uid']
         
         # Obter referência do Firestore
