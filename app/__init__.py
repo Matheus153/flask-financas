87b78981f060f19b28cb0d4c4d744c999839b8b8
Matheus153 <santiosmatheus@gmail.com>
2025-05-17 22:52:50 -0400

    fix: corrigindo SERVER_NAME e PREFERRED_URL_SCHEME

--- app/__init__.py
+++ app/__init__.py
@@ -48,8 +48,8 @@ def create_app():
     app.secret_key = os.getenv('SECRET_KEY')
 
     # Para usar nos links de direcionamentos de e-mail
-    app.config['SERVER_NAME'] = 'localhost' # Substitua por 'localhost' caso queira rodar localmente
-    app.config['PREFERRED_URL_SCHEME'] = 'http' # Substitua por 'http' caso queira rodar localmente
+    app.config['SERVER_NAME'] = os.getenv('SERVER_NAME') # Substitua por 'localhost' caso queira rodar localmente
+    app.config['PREFERRED_URL_SCHEME'] = os.getenv('PREFERRED_URL_SCHEME') # Substitua por 'http' caso queira rodar localmente
     app.config['APPLICATION_ROOT'] = '/'
 
     # Caso queira usar em ambiente local desmarque o comentario abaixo
